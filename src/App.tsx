e43a86795f9335fd287aa357737652522c024e9c
Bajuddin Khan <bajuddinkhan786@gmail.com>
2024-08-16 17:20:06 +0530

    app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,35 @@
-// import { useState } from "react";
-// import { TelnyxRTCProvider } from "@telnyx/react-client";
-// import ClientStatus from "./ClientStatus";
-// import CallLog from "./CallLog";
-// import Phone from "./Phone";
-// import VideoCall from "./VideoCall";
-import TelnyxPhone from "./TelnyxPhone";
+import { useState } from "react";
+import { TelnyxRTCProvider } from "@telnyx/react-client";
+import ClientStatus from "./ClientStatus";
+import CallLog from "./CallLog";
+import Phone from "./Phone";
+import VideoCall from "./VideoCall";
 
 const App = () => {
-  // const [credential, setCredential] = useState({
-  //   // Create a .env file with REACT_APP_TELNYX_LOGIN_TOKEN
-  //   // set to your On-Demand Credential Token to try this out
-  //   // https://developers.telnyx.com/docs/v2/webrtc/quickstart
-  //   login_token: import.meta.env.VITE_TELNYX_LOGIN_TOKEN || "mytoken",
-  // });
-  // const [loginTokenValue, setLoginTokenValue] = useState(
-  //   credential.login_token
-  // );
-  // const [isAudioOnly, setIsAudioOnly] = useState<boolean>(false);
+  const [credential, setCredential] = useState({
+    // Create a .env file with REACT_APP_TELNYX_LOGIN_TOKEN
+    // set to your On-Demand Credential Token to try this out
+    // https://developers.telnyx.com/docs/v2/webrtc/quickstart
+    login_token: import.meta.env.VITE_TELNYX_LOGIN_TOKEN || "mytoken",
+  });
+  const [loginTokenValue, setLoginTokenValue] = useState(
+    credential.login_token
+  );
+  const [isAudioOnly, setIsAudioOnly] = useState<boolean>(false);
 
-  // const handleSubmit = (e: any) => {
-  //   e.preventDefault();
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
 
-  //   setCredential({ login_token: loginTokenValue });
-  // };
-  // const options = {
-  //   ringtoneFile: "./sounds/ringback_tone.mp3",
-  //   ringbackFile: "./sounds/ringback_tone.mp3",
-  // };
+    setCredential({ login_token: loginTokenValue });
+  };
+  const options = {
+    ringtoneFile: "./sounds/ringback_tone.mp3",
+    ringbackFile: "./sounds/ringback_tone.mp3",
+  };
 
   return (
     <div style={{ padding: 20 }}>
-      {/* <TelnyxRTCProvider
+      <TelnyxRTCProvider
         credential={{
           login: "gencredOZpTCE3XqNinAi9HnQINeo2dYeftUx15KjOeWUHSxd",
           password: "d8470d1730bc4820ba4f96828b8621c5",
@@ -65,8 +64,7 @@ const App = () => {
 
           {isAudioOnly ? <Phone /> : <VideoCall />}
         </div>
-      </TelnyxRTCProvider> */}
-      <TelnyxPhone />
+      </TelnyxRTCProvider>
     </div>
   );
 };
